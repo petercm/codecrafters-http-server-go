ee7e93f795faea98f69dbfcdcf736dfd29f488e1
Peter Miller <petercm@gmail.com>
2024-06-05 13:41:51 -0400

    echo

--- app/server.go
+++ app/server.go
@@ -6,7 +6,30 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"strings"
 )
+
+func handle(conn net.Conn) {
+	request, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		fmt.Println("Error reading request.", err.Error())
+	}
+	fmt.Printf("Request: %s %s \n", request.Method, request.URL)
+	toEcho, isEcho := strings.CutPrefix(request.URL.Path, "/echo/")
+	if isEcho {
+		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n")
+		fmt.Fprintf(conn, "Content-Type: text/plain\r\n")
+		fmt.Fprintf(conn, "Content-Length: %d\r\n", len(toEcho))
+		fmt.Fprintf(conn, "\r\n") //headers
+		fmt.Fprintf(conn, "%s\r\n", toEcho)
+		return
+	}
+	if request.URL.Path == "/" {
+		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
+		return
+	}
+	fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+}
 
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -25,14 +48,5 @@ func main() {
 
 	}
 	defer conn.Close()
-	request, err := http.ReadRequest(bufio.NewReader(conn))
-	if err != nil {
-		fmt.Println("Error reading request.", err.Error())
-	}
-	fmt.Printf("Request: %s %s \n", request.Method, request.URL)
-	if request.URL.Path == "/" {
-		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
-		return
-	}
-	fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+	handle(conn)
 }
