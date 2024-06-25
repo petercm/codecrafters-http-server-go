b39abe6b9194947facce3d59e1e2bb2db874a829
Peter Miller <petercm@gmail.com>
2024-06-25 18:08:27 -0400

    Compression headers and some light refactoring

--- app/server.go
+++ app/server.go
@@ -10,20 +10,40 @@ import (
 	"strings"
 )
 
-func okContent(conn net.Conn, content string) {
-	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n")
-	fmt.Fprintf(conn, "Content-Type: text/plain\r\n")
-	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(content))
-	fmt.Fprintf(conn, "\r\n") //headers
-	fmt.Fprintf(conn, "%s\r\n", content)
+type HttpResponse struct {
+	out     io.Writer
+	status  string
+	headers map[string]string
+	content []byte
 }
 
-func okFile(conn net.Conn, content []byte) {
-	fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n")
-	fmt.Fprintf(conn, "Content-Type: application/octet-stream\r\n")
-	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(content))
-	fmt.Fprintf(conn, "\r\n") //headers
-	fmt.Fprintf(conn, "%s\r\n", content)
+func okString(resp HttpResponse, content string) {
+	resp.status = "200 OK"
+	resp.headers["Content-Type"] = "text/plain"
+	resp.headers["Content-Length"] = fmt.Sprint(len(content))
+	resp.content = []byte(content)
+	respond(resp)
+}
+
+func okFile(resp HttpResponse, content []byte) {
+	resp.status = "200 OK"
+	resp.headers["Content-Type"] = "application/octet-stream"
+	resp.headers["Content-Length"] = fmt.Sprint(len(content))
+	resp.content = content
+	respond(resp)
+}
+
+func respond(resp HttpResponse) {
+	fmt.Fprintf(resp.out, "HTTP/1.1 %s\r\n", resp.status)
+	if len(resp.headers) > 0 {
+		for header, value := range resp.headers {
+			fmt.Fprintf(resp.out, "%s: %s\r\n", header, value)
+		}
+	} else {
+		fmt.Fprintf(resp.out, "\r\n")
+	}
+	fmt.Fprintf(resp.out, "\r\n") // end headers
+	fmt.Fprintf(resp.out, "%s\r\n", resp.content)
 }
 
 func handle(conn net.Conn) {
@@ -33,10 +53,15 @@ func handle(conn net.Conn) {
 		fmt.Println("Error reading request.", err.Error())
 	}
 	fmt.Printf("Request: %s %s \n", request.Method, request.URL)
+	resp := HttpResponse{out: conn, headers: make(map[string]string)}
+	acceptEncoding := request.Header["Accept-Encoding"]
+	if len(acceptEncoding) > 0 && acceptEncoding[0] == "gzip" {
+		resp.headers["Content-Encoding"] = "gzip"
+	}
 
 	toEcho, isEcho := strings.CutPrefix(request.URL.Path, "/echo/")
 	if isEcho {
-		okContent(conn, toEcho)
+		okString(resp, toEcho)
 		return
 	}
 
@@ -56,28 +81,32 @@ func handle(conn net.Conn) {
 			if err != nil {
 				panic(err)
 			}
-			fmt.Fprintf(conn, "HTTP/1.1 201 Created\r\n\r\n")
+			resp.status = "201 Created"
+			respond(resp)
 			return
 		} else {
 
 			fileContent, err := os.ReadFile(os.Args[2] + fileName)
 			if err != nil {
 				fmt.Println("Error reading request.", err.Error())
-				fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+				resp.status = "404 Not Found"
+				respond(resp)
 				return
 			}
-			okFile(conn, fileContent)
+			okFile(resp, fileContent)
 			return
 		}
 	}
 
 	switch request.URL.Path {
 	case "/":
-		fmt.Fprintf(conn, "HTTP/1.1 200 OK\r\n\r\n")
+		resp.status = "200 OK"
+		respond(resp)
 	case "/user-agent":
-		okContent(conn, request.UserAgent())
+		okString(resp, request.UserAgent())
 	default:
-		fmt.Fprintf(conn, "HTTP/1.1 404 Not Found\r\n\r\n")
+		resp.status = "404 Not Found"
+		respond(resp)
 	}
 
 }
